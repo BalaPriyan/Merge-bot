6ae2ffac7e3b40ce3bf44f492d844b97ebeb5372
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-12-06 20:33:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
+    API_HASH = os.environ.get("API_HASH", "8c03cfc6e8be855de935ac61ac4047d7")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6539803331:AAHLOjtJg74ehDrzwPZ5QKALLkXm7XeAwSk")
+    TELEGRAM_API = os.environ("TELEGRAM_API", "28583769")
+    OWNER = os.environ.get("OWNER", "6675988213")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "NoMoreFools")
+    PASSWORD = os.environ.get("PASSWORD", "TomenMreger")
     DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    LOGCHANNEL = os.environ.get("LOGCHANNEL", "-1002120946369")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
